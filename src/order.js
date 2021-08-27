a20ebf4d2482b730a654279a5e9dea29e93ee7f9
Kyra Bergsund <k.bergsund@gmail.com>
2021-08-26 20:38:45 -0700

    Complete order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
 
+function takeOrder(order, ordersArray) {
+  if (ordersArray.length < 3) {
+    ordersArray.push(order)
+  }
+  return ordersArray;
+}
+
+function refundOrder(orderToRemove, ordersArray) {
+  for (var i = 0; i < ordersArray.length; i++) {
+    if (ordersArray[i].orderNumber === orderToRemove) {
+      ordersArray.splice(i, 1)
+    }
+  }
+}
+
+function listItems(ordersArray) {
+  listArray = []
+  for (var i = 0; i < ordersArray.length; i++) {
+    listArray.push(ordersArray[i].item)
+  }
+  return listArray.join(", ")
+}
+
+function searchOrder(ordersArray, item) {
+  var inList = false
+  for (var i = 0; i < ordersArray.length; i++) {
+    if (ordersArray[i].item === item) {
+      inList = true
+    }
+  }
+  return inList
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
