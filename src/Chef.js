f54d8dff8135abe2f72fa0638a2b5cb850adfadf
Kyra Bergsund <k.bergsund@gmail.com>
2021-08-29 21:24:24 -0700

    Complete extension: Chef tests

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,30 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name
+    this.restaurant = restaurant
+  }
+
+  greetCustomer(customer, time) {
+    if (time === true) {
+      return `Good morning, ${customer}!`
+    } else {
+      return `Hello, ${customer}!`
+    }
+  }
+
+  checkForFood(item) {
+    // Another solution that only works if 'length === 0'. Struggling with working empty arrays.
+    if (this.restaurant.menus.breakfast.length === 0) {
+      return `Sorry, we aren't serving ${item.name} today.`
+    } else {
+      for (var i = 0; i < this.restaurant.menus.breakfast.length; i++) {
+        if (this.restaurant.menus.breakfast[i].name === item.name) {
+          return `Yes, we're serving ${item.name} today!`
+        }
+      }
+    }
   }
 }
+
 
 module.exports = Chef;
